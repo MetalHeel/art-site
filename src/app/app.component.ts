32f8f087843aaea486c96907d274eb71d54e5d82
MetalHeel <MetalHeel@aol.com>
2025-05-21 14:06:18 -0500

    Added some small updates to make things pretty.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: `
-    Future Home for Laura's Website
-  `
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+}
